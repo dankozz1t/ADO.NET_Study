d1a0e006f1b829b0555d387bf2eb7088cc2e50e1
Alex <AlexDankoXXL@gmail.com>
2022-02-20 00:04:41 +0200

    add entities

---
+++ DZ_07_CodeFirstPhoneBook/Entities/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DZ_07_CodeFirstPhoneBook.Entities
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+        public short Priority { get; set; }
+        public bool IsBlocking { get; set; }
+
+        public int GroupId { get; set; }
+        public virtual Group Group { get; set; }
+    }
+}
---
+++ DZ_07_CodeFirstPhoneBook/Entities/Group.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DZ_07_CodeFirstPhoneBook.Entities
+{
+    public class Group
+    {
+        public Group()
+        {
+            this.Contacts = new HashSet<Contact>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Contact> Contacts { get; set; }
+    }
+}
