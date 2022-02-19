7fb594e9044083da64539e35db2861dce70d7c59
Alex <AlexDankoXXL@gmail.com>
2022-02-20 00:03:54 +0200

    add DbContext

---
+++ DZ_07_CodeFirstPhoneBook/DbContext/PhoneBookDb.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using DZ_07_CodeFirstPhoneBook.Entities;
+
+namespace DZ_07_CodeFirstPhoneBook.DbContext
+{
+    public class PhoneBookDb : System.Data.Entity.DbContext
+    {
+        public PhoneBookDb(string connectionString) : base(connectionString) { }
+        public PhoneBookDb() : base("PhoneBookDbConnectionString") { }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Group> Groups { get; set; }
+    }
+
+}
