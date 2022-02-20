2ddb3f90b4617fa65a16065cbc35a626d521b9c3
Alex <AlexDankoXXL@gmail.com>
2022-02-20 19:09:29 +0200

    fuck database

--- DZ_07_CodeFirstPhoneBook/DbContext/PhoneBookDb.cs
+++ DZ_07_CodeFirstPhoneBook/DbContext/PhoneBookDb.cs
@@ -1,12 +1,24 @@
-﻿using System.Data.Entity;
-using DZ_07_CodeFirstPhoneBook.Entities;
+﻿using System;
+using System.Data.Entity;
+using System.IO;
+using DZ_07_CodeFirstPhoneBook.Helpers;
 
 namespace DZ_07_CodeFirstPhoneBook.DbContext
 {
     public class PhoneBookDb : System.Data.Entity.DbContext
     {
         public PhoneBookDb(string connectionString) : base(connectionString) { }
-        public PhoneBookDb() : base("PhoneBookDbConnectionString") { }
+        public static string MDF_Directory
+        {
+            get
+            {
+                var directoryPath = AppDomain.CurrentDomain.BaseDirectory;
+                return Path.GetFullPath(Path.Combine(directoryPath, "..//PhoneBook.mdf"));
+            }
+        }
+        public PhoneBookDb() : base($@"Data Source=(LocalDB)\MSSQLLocalDB; " +
+                                    "AttachDbFilename=" + MDF_Directory +
+                                    " Integrated Security=True; Connect Timeout=30;") { }
 
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<Group> Groups { get; set; }
