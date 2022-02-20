f6d78c2ef8e029a3d732cfd4166b7b977a0f12c1
Alex <AlexDankoXXL@gmail.com>
2022-02-20 19:49:45 +0200

    fix database

--- DZ_07_CodeFirstPhoneBook/DbContext/PhoneBookDb.cs
+++ DZ_07_CodeFirstPhoneBook/DbContext/PhoneBookDb.cs
@@ -8,8 +8,19 @@ namespace DZ_07_CodeFirstPhoneBook.DbContext
     public class PhoneBookDb : System.Data.Entity.DbContext
     {
         public PhoneBookDb(string connectionString) : base(connectionString) { }
-   
-        public PhoneBookDb() : base($@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={Environment.CurrentDirectory}\\PhoneBook.mdf;Integrated Security=True; Connect Timeout=30;") {}
+        public static string MDF_Directory
+        {
+            get
+            {
+                string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+                string s = Environment.CurrentDirectory;
+
+                var directoryPath = AppDomain.CurrentDomain.BaseDirectory;
+                return Path.GetFullPath(Path.Combine(directoryPath, @"..\..\PhoneBook.mdf"));
+            }
+        }
+        public PhoneBookDb() : base($@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={MDF_Directory};Integrated Security=True; Connect Timeout=30;") {}
 
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<Group> Groups { get; set; }
