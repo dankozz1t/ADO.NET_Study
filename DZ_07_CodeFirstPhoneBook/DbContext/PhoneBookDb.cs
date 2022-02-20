eaa9073e8bf084e5eb26c8d1cd5191a488002272
Alex <AlexDankoXXL@gmail.com>
2022-02-20 19:39:28 +0200

    Update PhoneBookDb.cs

--- DZ_07_CodeFirstPhoneBook/DbContext/PhoneBookDb.cs
+++ DZ_07_CodeFirstPhoneBook/DbContext/PhoneBookDb.cs
@@ -8,17 +8,8 @@ namespace DZ_07_CodeFirstPhoneBook.DbContext
     public class PhoneBookDb : System.Data.Entity.DbContext
     {
         public PhoneBookDb(string connectionString) : base(connectionString) { }
-        public static string MDF_Directory
-        {
-            get
-            {
-                var directoryPath = AppDomain.CurrentDomain.BaseDirectory;
-                return Path.GetFullPath(Path.Combine(directoryPath, "..//PhoneBook.mdf"));
-            }
-        }
-        public PhoneBookDb() : base($@"Data Source=(LocalDB)\MSSQLLocalDB; " +
-                                    "AttachDbFilename=" + MDF_Directory +
-                                    " Integrated Security=True; Connect Timeout=30;") { }
+   
+        public PhoneBookDb() : base($@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={Environment.CurrentDirectory}\\PhoneBook.mdf;Integrated Security=True; Connect Timeout=30;") {}
 
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<Group> Groups { get; set; }
